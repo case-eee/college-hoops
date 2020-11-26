6f39a58edc5d08d0156c1c2adec3c6d2d484e50b
Casey Cumbow <cumbow8@gmail.com>
2020-11-26 09:21:43 -0500

    Add files

---
+++ db/migrate/20201126135532_add_users.rb
@@ -0,0 +1,13 @@
+class AddUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_hash
+      t.boolean :paid
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
